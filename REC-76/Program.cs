d743f4c1e91c500265c345e8fd941bfd50e7f137
сергей токарев <sseregatokarew@gmail.com>
2021-11-19 15:47:00 +0700

    Добавьте файлы проекта.

---
+++ REC-76/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace REC_76
+{
+    class Program
+    {
+        private static int[,] Matrix(int n, int m)
+        {
+            int[,] ARR = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    ARR[i, j] = 0;
+                }
+            }
+            return ARR;
+        }
+        static void PrintMatrix(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write(m[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк матрицы: ");
+            var Row = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов матрицы: ");
+            var Column = int.Parse(Console.ReadLine());
+            PrintMatrix(Matrix(Row, Column));
+            Console.ReadKey();
+        }
+    }
+}
